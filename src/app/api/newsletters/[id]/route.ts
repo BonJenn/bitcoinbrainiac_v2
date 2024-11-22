cadbad01d091265e0463412086e91a54cffa1f3e
Jonathan Benn <bonjennprojects@gmail.com>
2024-11-21 17:56:18 -0800

    Added feed and fixed scheduling

--- src/app/api/newsletters/[id]/route.ts
+++ src/app/api/newsletters/[id]/route.ts
@@ -2,14 +2,18 @@ import { NextResponse } from 'next/server';
 import { connectToDatabase } from '@/lib/db';
 import Newsletter from '@/models/Newsletter';
 
+type Props = {
+  params: { id: string }
+}
+
 export async function GET(
-  request: Request,
-  context: { params: { id: string } }
+  _request: Request,
+  { params }: Props
 ) {
   try {
     await connectToDatabase();
     
-    const newsletter = await Newsletter.findOne({ id: context.params.id });
+    const newsletter = await Newsletter.findOne({ id: params.id });
     
     if (!newsletter) {
       return NextResponse.json(
