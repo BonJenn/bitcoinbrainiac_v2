7bac570967cd5dc16cfad168385b415397d34642
Jonathan Benn <bonjennprojects@gmail.com>
2024-11-21 19:11:51 -0800

    fixing feed

--- src/types/mailchimp.d.ts
+++ src/types/mailchimp.d.ts
@@ -4,6 +4,14 @@ declare module '@mailchimp/mailchimp_marketing' {
     server: string | undefined;
   }
 
+  interface Campaign {
+    id: string;
+    type: string;
+    status: string;
+    settings: any;
+    recipients: any;
+  }
+
   interface MailchimpClient {
     setConfig: (config: MailchimpConfig) => void;
     lists: {
@@ -11,6 +19,14 @@ declare module '@mailchimp/mailchimp_marketing' {
       updateListMember: (listId: string, email: string, data: any) => Promise<any>;
       addListMember: (listId: string, data: any) => Promise<any>;
       deleteListMember: (listId: string, subscriberHash: string) => Promise<any>;
+    };
+    campaigns: {
+      create: (data: any) => Promise<Campaign>;
+      setContent: (campaignId: string, data: { html: string }) => Promise<any>;
+      send: (campaignId: string) => Promise<any>;
+    };
+    messages: {
+      send: (data: any) => Promise<any>;
     };
   }
 
