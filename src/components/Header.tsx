250b278d4f34a0e4c77e036c19128576928c7414
Jonathan Benn <bonjennprojects@gmail.com>
2025-03-13 11:43:35 -0700

    Fixed header issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="p-4 pl-8 md:p-6 md:pl-20">
-      <Link href="/" className="text-xl font-bold text-black">
-        Bitcoin Brainiac
-      </Link>
+    <header className="bg-gradient-to-br from-[#ffe4bc] to-[#ffc49d]">
+      <div className="container mx-auto p-4 pl-8 md:p-6 md:pl-20">
+        <Link href="/" className="text-xl font-bold text-[#f97316]">
+          Bitcoin Brainiac
+        </Link>
+      </div>
     </header>
   );
 }
