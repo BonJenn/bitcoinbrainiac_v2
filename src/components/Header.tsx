a3d4ae2a414b0526fbf3cce30157b04b95ea83c1
Jonathan Benn <bonjennprojects@gmail.com>
2025-03-13 11:44:51 -0700

    Fixed header again

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="bg-gradient-to-br from-[#ffe4bc] to-[#ffc49d]">
+    <header className="bg-transparent">
       <div className="container mx-auto p-4 pl-8 md:p-6 md:pl-20">
         <Link href="/" className="text-xl font-bold text-[#f97316]">
           Bitcoin Brainiac
